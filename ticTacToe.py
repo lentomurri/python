2737bd1fb6678287b3b7a6f3535aab1989041d98
lento <[dibonaventurasilvia@gmail.com]>
2021-02-21 15:29:19 +0000

    Tic Tac game

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,67 +1,58 @@
 import random
 
 # SESSION WITH BOARD AND POSITIONS TO UPDATE
-positions = [1,2,3,4,5,6,7,8,9]
-win = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-moves = 0
-# END SESSION
 
-def display_board():
-    top = "+-----+-----+-----+"
-    side = "|     |     |     |"
-    board = f"""
+class Manager():
+    #class instances available to be modified
+    positions = [1,2,3,4,5,6,7,8,9]
+    moves = 0
+    win = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+    #creates board
+    def __init__(self):
+        top = "+-----+-----+-----+"
+        side = "|     |     |     |"
+        self.board = f"""
 {top}
 {side}
-|  {positions[0]}  |  {positions[1]}  |  {positions[2]}  |
+|  {Manager.positions[0]}  |  {Manager.positions[1]}  |  {Manager.positions[2]}  |
 {side}
 {top}
 {side}
-|  {positions[3]}  |  {positions[4]}  |  {positions[5]}  |
+|  {Manager.positions[3]}  |  {Manager.positions[4]}  |  {Manager.positions[5]}  |
 {side}
 {top}
 {side}
-|  {positions[6]}  |  {positions[7]}  |  {positions[8]}  |
+|  {Manager.positions[6]}  |  {Manager.positions[7]}  |  {Manager.positions[8]}  |
 {side}
 {top}
 """
-    print(board)
 
+#PLAYER MOVE
 
-def enter_move():
-    user = int(input("Enter your number from 1 to 9 > "))
+def enter_move(positions):
+    # handles user inputs higher than 9 and not-integer
     while True:
-        if user not in positions:
-            make_list_of_free_fields()
-        else:
+        try:
+            user = int(input("Enter your number from 1 to 9 > "))
             ind = positions.index(user)
             positions[ind] = "O"
             break
+        except ValueError:
+            print("Please enter a valid number")
 
-def make_list_of_free_fields():
+#PRINTS AVAILABLE SPACES
+
+def make_list_of_free_fields(positions):
     print("Available Positions: ")
     for i in positions:
-        if not i == "O" or not i=="X":
+        if type(i) == int:
             print(i, end=" ")
 
-def win_move():
-    for i in range(0,9):
-        for j in range(0,3):
-            counterO = 0
-            counterX = 0
-            if counterO == 3:
-                return "Player wins!"
-            if counterX == 3:
-                return "Computer wins!"
-            if win[i][j] == "O":
-                counterO += 1
-            elif win[i][j] == "X":
-                counterX +=1
-    return "None won! Keep going!"
+#CHECKS IF PLAYER OR CPU WON
 
-
-def victory_for():
-    # The function analyzes the board status in order to check if 
-    # the player using 'O's or 'X's has won the game
+def victory_for(positions, win):
+    # The function analyzes the board status compared to the win options
     for i in range(0,8):
         counterO = 0
         counterX = 0
@@ -74,12 +65,11 @@ def victory_for():
             return "Player wins!"
         if counterX == 3:
             return "Computer wins!"
-    return "None won! Keep going!"
+    return False
 
-def draw_move():
-    # The function draws the computer's move and updates the board.
+def draw_move(moves, positions):
+    # CPU moves randomly and updates the board.
     while True:
-        global moves
         comp = random.randint(1,9)
         if comp in positions:
             ind = positions.index(comp)
@@ -88,16 +78,20 @@ def draw_move():
             break
 
 while True:
-    display_board()
-    make_list_of_free_fields()
-    enter_move()
-    display_board()
+    #the class variables used to perform the checks 
+    board = Manager().board
+    positions = Manager().positions
+    win = Manager().win
+    moves = Manager().moves
+    print(board)
+    make_list_of_free_fields(positions)
+    enter_move(positions)
+    print(board)
     if moves == 9:
         print("It's a tie!")
         break
-    check = victory_for()
-    if check != "None won! Keep going!":
+    check = victory_for(positions, win)
+    if check:
         print(check)
         break
-    draw_move()
-    display_board()
+    draw_move(moves, positions)
