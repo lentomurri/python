f1da91974f5accb72e4c1fec1c12474f85cc9e13
S <dibonaventurasilvia@gmail.com>
2021-05-02 10:36:17 +0100

    Delete MILES_TO_GALLON.py

--- MILES_TO_GALLON.py
+++
@@ -1,7 +0,0 @@
-﻿def liters_100km_to_miles_gallon(liters):
-    return (100 / 1.609344) / ((liters / 3.785411784))
-
-
-def miles_gallon_to_liters_100km(miles):
-    #convert miles gallon to a liter #
-    return 100 / (miles * 1.609344) * 3.785411784
