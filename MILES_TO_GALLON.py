54aba959d04d1b141cfda28e8a604e106993e2ce
S <dibonaventurasilvia@gmail.com>
2021-05-02 10:26:23 +0100

    Update MILES_TO_GALLON.py

--- MILES_TO_GALLON.py
+++ MILES_TO_GALLON.py
@@ -5,10 +5,3 @@
 def miles_gallon_to_liters_100km(miles):
     #convert miles gallon to a liter #
     return 100 / (miles * 1.609344) * 3.785411784
-
-print(liters_100km_to_miles_gallon(3.9))
-print(liters_100km_to_miles_gallon(7.5))
-print(liters_100km_to_miles_gallon(10.))
-print(miles_gallon_to_liters_100km(60.3))
-print(miles_gallon_to_liters_100km(31.4))
-print(miles_gallon_to_liters_100km(23.5))
