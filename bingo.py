ceabd5a384b422396058a09850861f0b10c3f981
S <dibonaventurasilvia@gmail.com>
2021-05-02 12:07:04 +0100

    Update bingo.py

--- bingo.py
+++ bingo.py
@@ -1,11 +1,11 @@
-﻿from random import sample 
+# A little bingo for boring Quarantine time. 
+#Little exercise to memorise sample functions.
+
+from random import sample 
 from time import sleep
 
-list = [i for i in range (1,11)]
-print(list)
-bingo = sample(list, 5)
+print("Next number is...")
 
-for i in bingo: 
-    print("Next number is...")
-    print(i, end = " \r")
+for i in sample([i for i in range (1,90)], 89): 
+    print(i)
     sleep(2)
